385b291d4ff3cd9f8ab69af8e75cb561037ab8b2
Lucas Natario <lucasenatario@gmail.com>
2022-01-12 23:42:17 -0300

    commit inicial

---
+++ app/Console/Commands/ListenMentions.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Atymic\Twitter\Facade\Twitter;
+use Illuminate\Console\Command;
+
+class ListenMentions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'listen:mentions';
+
+    private $paramsResponse =
+        [
+            "tweet.fields" => "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang," .
+                "public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld"
+            ,
+            "user.fields" =>
+                "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username," .
+                "verified,withheld"
+
+        ];
+
+    private $termToSearchPattern = '/"(?<term>[a-zA-z0-9]+)"/';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Twitter::getStream(function ($tweet) {
+
+            /** Decode tweet JSON to array */
+            $tweetArray = json_decode(utf8_decode($tweet), true);
+
+            /** Verify if it is a hearthbeat to keep alive the stream */
+            if (empty($tweetArray)) {
+                $this->info("hearthbeat");
+                return;
+            }
+
+            /** Extract the tweet text */
+            $tweetText = $tweetArray['data']['text'];
+
+            /** Proccess tweet text with the predefined regex pattern to obtain the term to search*/
+            preg_match($this->termToSearchPattern, $tweetText, $termMatch);
+            $termToSearch = $termMatch['term'] ?? null;
+
+            /** Verify if there is any term to search */
+            if (!empty($termToSearch))
+                $this->info("Term to search: " . $termToSearch);
+            else
+                $this->info("No term to search in this tweet");
+
+        },
+            $this->paramsResponse
+        );
+    }
+}
